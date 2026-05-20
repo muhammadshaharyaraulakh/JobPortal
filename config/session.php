fa4995166c81c84b7f584e5060409770f52f8940
muhammadshaharyaraulakh <muhammadshaharyaraulakh@gmail.com>
2026-05-20 20:26:53 +0500

    Final GitHub contribution fix

--- config/session.php
+++ config/session.php
@@ -189,7 +189,7 @@ return [
     | Same-Site Cookies
     |--------------------------------------------------------------------------
     |
-    | This option determines how your cookies behave when cross-site requests
+    | This  determines how your cookies behave when cross-site requests
     | take place, and can be used to mitigate CSRF attacks. By default, we
     | will set this value to "lax" to permit secure cross-site requests.
     |
